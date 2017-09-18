3f7111c454fb053b31c6955217f954f0cdc7a914
Myron Anthony <codehearted12@gmail.com>
2017-09-17 23:58:51 -0400

    commit final

--- src/components/GetImageform.js
+++ src/components/GetImageform.js
@@ -3,7 +3,7 @@ import GetImageButton from './GetImageButton'
 import ImageDisplay from './ImageDisplay'
 const API_KEY = 'TcFyYuuGotEOTLcS2EnWdjiADxBy3dyNdJ4QP3cE'
 
-export default class GetImageForm extends Component{
+class GetImageForm extends Component{
   constructor(props){
     super(props);
     this.state ={
@@ -17,18 +17,16 @@ this.handleRover = this.handleRover.bind(this)
 this.handleCamera = this.handleCamera.bind(this)
 this.handleSol = this.handleSol.bind(this)
 }
-componentDidMount(){
-  this.fetchRoverImage()
-}
+
 componentDidMount(){
   this.fetchRoverImage()
 }
 fetchRoverImage(){
   this.setState({camera: this.state.camera, rover: this.state.rover, sol: this.state.sol});
   let cam = this.state.camera
-  let rove = this.state.rover
-  let num = this.state.sol
-  let imageUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=${num}&camera=${cam}&api_key=${API_KEY}`;
+  let rover = this.state.rover
+  let sol = this.state.sol
+  let imageUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=${sol}&camera=${cam}&api_key=${API_KEY}`;
   fetch(imageUrl)
   .then(res => res.json())
   .then(json =>{
@@ -63,7 +61,7 @@ fetchRoverImage(){
           <select onChange={this.handleRover} id="rover" value={this.state.rover}>
           <option value="Curiosity">Curiosity</option>
           <option value="Opportunity">Opportunity</option>
-                  <option value="Spirit">Spirt</option>
+                  <option value="Spirit">Spirit</option>
                 </select>
                   <label htmlFor="camera">Camera Type</label>
                   <select onChange={this.handleCamera} id="camera" value={this.state.camera}>
