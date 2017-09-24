f73c689d1853fa5a11ec6769ee8ccbddc6367396
Myron Anthony <codehearted12@gmail.com>
2017-09-23 22:27:27 -0400

    commit

--- src/components/GetImageform.js
+++ src/components/GetImageform.js
@@ -3,7 +3,7 @@ import GetImageButton from './GetImageButton'
 import ImageDisplay from './ImageDisplay'
 const API_KEY = 'TcFyYuuGotEOTLcS2EnWdjiADxBy3dyNdJ4QP3cE'
 
-class GetImageForm extends Component{
+class GetImageForm extends Component {
   constructor(props){
     super(props);
     this.state ={
@@ -23,7 +23,7 @@ componentDidMount(){
 }
 fetchRoverImage(){
   this.setState({camera: this.state.camera, rover: this.state.rover, sol: this.state.sol});
-  let cam = this.state.camera;                                                                                                                                          
+  let cam = this.state.camera;
   let rover = this.state.rover
   let sol = this.state.sol
   let imageUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=${sol}&camera=${cam}&api_key=${API_KEY}`;
